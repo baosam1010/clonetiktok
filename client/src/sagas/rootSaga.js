ac8cdb13fe1b4d9b2a70f45254e5295ab46f30eb
baosam1010 <baosam1010@gmail.com>
2021-12-16 15:39:07 +0700

    configureStore

---
+++ client/src/sagas/rootSaga.js
@@ -0,0 +1,41 @@
+
+import {
+//   call,
+//   delay,
+  fork,
+//   put,
+//   select,
+//   take,
+//   takeEvery,
+//   takeLatest,
+} from 'redux-saga/effects';
+
+
+/**
+ * B1: Thực thi action fetch task
+ * B2: Gọi api
+ * B2.1: Hiển thị thanh tiến trình ( loading )
+ * B3: Kiểm tra status code
+ * Nếu thành công...
+ * Nếu thất bại...
+ * B4: Tắt loading
+ * B5: Thực thi các công việc tiếp theo
+ */
+
+
+
+
+
+
+function* watchFetchListTaskAction() {
+  console.log('Root Saga');
+  yield true;
+}
+
+function* rootSaga() {
+  yield fork(watchFetchListTaskAction);
+
+}
+
+export default rootSaga;
+
